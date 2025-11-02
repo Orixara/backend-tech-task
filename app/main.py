3b2d50ea5f7ca89078ee47c28b973bde0ca21968
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-11-02 18:22:24 +0200

    included events_router in main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from contextlib import asynccontextmanager
 from database import close_redis_pool, get_redis_pool
 from fastapi import FastAPI
 
+from routes import events_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,6 +20,7 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+app.include_router(events_router)
 
 @app.get("/health")
 async def health_check():
