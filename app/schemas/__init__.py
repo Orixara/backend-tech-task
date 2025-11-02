8f22d87f3e2f6fa8ba27775665cedda70f350127
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-11-02 18:21:48 +0200

    added imports in directory schemas in __init__.py

---
+++ app/schemas/__init__.py
@@ -0,0 +1,23 @@
+from .event import (
+    EventCreateRequestSchema,
+    EventsBatchCreateRequestSchema,
+    EventsBatchResponseSchema,
+    DAUItemSchema,
+    DAUResponseSchema,
+    TopEventItemSchema,
+    TopEventsResponseSchema,
+    RetentionCohortSchema,
+    RetentionResponseSchema,
+)
+
+__all__ = [
+    "EventCreateRequestSchema",
+    "EventsBatchCreateRequestSchema",
+    "EventsBatchResponseSchema",
+    "DAUItemSchema",
+    "DAUResponseSchema",
+    "TopEventItemSchema",
+    "TopEventsResponseSchema",
+    "RetentionCohortSchema",
+    "RetentionResponseSchema",
+]
