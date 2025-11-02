71079809eeb334d00b7ef4741ca13b9a4bb42ecc
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-11-02 15:41:31 +0200

    Corrected imports in env.py

--- app/database/migrations/env.py
+++ app/database/migrations/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-from app.config.settings import settings
+from config.settings import settings
 
 config = context.config
 
